b3a0129af834d41a655a612d6c6b9c2cc8f1cf85
Ardian0311 <231230061@unmuhpnk.ac.id>
2025-05-19 19:44:43 +0700

    MembuatFormContact

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ContactController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route untuk halaman kontak
+Route::get('/contact', [ContactController::class, 'index']);
+Route::post('/contact', [ContactController::class, 'store']); 
 
-Route::get('/', [ProductController::class, 'index']);
+// Route untuk menampilkan form user
+Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
 
-Route::get('/product/{id}', [ProductController::class, 'show']);
+// Route untuk menyimpan data user dari form
+Route::post('/user', [UserController::class, 'store'])->name('user.store');
+Route::resource('user', UserController::class);
+Route::resource('product', ProductController::class);
